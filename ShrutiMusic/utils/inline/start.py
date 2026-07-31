4acc15999c35cf3bd603877c9836fa4ddbc6f0da
ukamanboss <ukamanboss@gmail.com>
2026-07-31 16:47:08 +0530

    start.py

--- ShrutiMusic/utils/inline/start.py
+++ ShrutiMusic/utils/inline/start.py
@@ -5,6 +5,7 @@
 
 from pyrogram.types import InlineKeyboardButton
 from pyrogram.enums import ButtonStyle
+
 import config
 from ShrutiMusic import app
 
@@ -15,27 +16,28 @@ def start_panel(_):
             InlineKeyboardButton(
                 text=_["S_B_1"],
                 url=f"https://t.me/{app.username}?startgroup=true",
-                style=ButtonStyle.SUCCESS
+                style=ButtonStyle.SUCCESS,
             ),
             InlineKeyboardButton(
                 text=_["S_B_2"],
                 url=config.SUPPORT_GROUP,
-                style=ButtonStyle.PRIMARY
+                style=ButtonStyle.PRIMARY,
             ),
         ],
         [
             InlineKeyboardButton(
                 text=_["E_X_1"],
                 url=config.UPSTREAM_REPO,
-                style=ButtonStyle.PRIMARY
+                style=ButtonStyle.PRIMARY,
             ),
             InlineKeyboardButton(
                 text=_["S_B_11"],
                 callback_data="about_page",
-                style=ButtonStyle.SUCCESS
-            )
+                style=ButtonStyle.PRIMARY,
+            ),
         ],
     ]
+
     return buttons
 
 
@@ -45,41 +47,42 @@ def private_panel(_):
             InlineKeyboardButton(
                 text=_["S_B_3"],
                 url=f"https://t.me/{app.username}?startgroup=true",
-                style=ButtonStyle.SUCCESS
+                style=ButtonStyle.SUCCESS,
             )
         ],
         [
             InlineKeyboardButton(
                 text=_["S_B_11"],
                 callback_data="about_page",
-                style=ButtonStyle.PRIMARY
+                style=ButtonStyle.PRIMARY,
             ),
             InlineKeyboardButton(
                 text=_["S_B_12"],
                 callback_data="owner_page",
-                style=ButtonStyle.PRIMARY
-            )
+                style=ButtonStyle.PRIMARY,
+            ),
         ],
         [
             InlineKeyboardButton(
                 text=_["E_X_1"],
                 callback_data="fork_repo",
-                style=ButtonStyle.PRIMARY
+                style=ButtonStyle.PRIMARY,
             ),
             InlineKeyboardButton(
                 text=_["S_B_5"],
                 user_id=config.OWNER_ID,
-                style=ButtonStyle.SUCCESS
+                style=ButtonStyle.SUCCESS,
             ),
         ],
         [
             InlineKeyboardButton(
                 text=_["S_B_4"],
                 callback_data="help_page_1",
-                style=ButtonStyle.PRIMARY
+                style=ButtonStyle.PRIMARY,
             )
         ],
     ]
+
     return buttons
 
 
@@ -89,22 +92,23 @@ def about_panel(_):
             InlineKeyboardButton(
                 text=_["S_B_6"],
                 url=config.SUPPORT_CHANNEL,
-                style=ButtonStyle.PRIMARY
+                style=ButtonStyle.PRIMARY,
             ),
             InlineKeyboardButton(
                 text=_["S_B_2"],
                 url=config.SUPPORT_GROUP,
-                style=ButtonStyle.SUCCESS
+                style=ButtonStyle.SUCCESS,
             ),
         ],
         [
             InlineKeyboardButton(
                 text=_["BACK_BUTTON"],
                 callback_data="settingsback_helper",
-                style=ButtonStyle.PRIMARY
+                style=ButtonStyle.PRIMARY,
             )
-        ]
+        ],
     ]
+
     return buttons
 
 
@@ -114,32 +118,33 @@ def owner_panel(_):
             InlineKeyboardButton(
                 text=_["S_H_1"],
                 url=config.INSTAGRAM,
-                style=ButtonStyle.PRIMARY
+                style=ButtonStyle.PRIMARY,
             ),
             InlineKeyboardButton(
                 text=_["S_H_2"],
                 url=config.YOUTUBE,
-                style=ButtonStyle.DANGER
+                style=ButtonStyle.DANGER,
             ),
         ],
         [
             InlineKeyboardButton(
                 text=_["S_H_3"],
                 url=config.GITHUB,
-                style=ButtonStyle.PRIMARY
+                style=ButtonStyle.PRIMARY,
             ),
             InlineKeyboardButton(
                 text=_["S_H_4"],
                 url=config.DONATE,
-                style=ButtonStyle.SUCCESS
+                style=ButtonStyle.SUCCESS,
             ),
         ],
         [
             InlineKeyboardButton(
                 text=_["BACK_BUTTON"],
                 callback_data="settingsback_helper",
-                style=ButtonStyle.PRIMARY
+                style=ButtonStyle.PRIMARY,
             )
-        ]
+        ],
     ]
+
     return buttons
